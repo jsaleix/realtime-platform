525f8a6f5c4c7b901839bc68394873938824dd27
MaximeMalecot <92maximemalecot@gmail.com>
2022-11-20 16:24:08 +0100

    Modified Docker to launch hot reload on back

--- server/models/index.js
+++ server/models/index.js
@@ -11,8 +11,5 @@ exports.User.hasMany(exports.Message, { foreignKey: 'sender'});
 exports.Message.belongsTo(exports.Room);
 exports.Room.hasMany(exports.Message);
 
-exports.Room.belongsToMany(exports.User, { through: 'UserRoom' });
-exports.User.belongsToMany(exports.Room, { through: 'UserRoom' });
-
 exports.Conversation.belongsToMany(exports.User, { through: 'conversation_user' });
 exports.User.belongsToMany(exports.Conversation, { through: 'conversation_user' });
