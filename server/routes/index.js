8b010ec8cbde7aaaf6ddac18ca1a50823783b2cf
MaximeMalecot <92maximemalecot@gmail.com>
2022-11-20 16:07:20 +0100

    Modified nodemon for dockerfile

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,6 +5,6 @@ router.use('/user', require('./user'));
 router.use('/notification', require('./notification'));
 router.use('/message', require('./message'));
 router.use('/room', require('./room'));
-router.get('/', (req, res) => { return res.status(200).json({ message: "Welcome to the API" }) });
+router.get('/', (req, res) => { return res.status(200).json({ message: "Welcto the API azaeaze" }) });
 
 module.exports = router;
