79c770bcc97e586faf8f92df6530fc269627bb1b
MaximeMalecot <92maximemalecot@gmail.com>
2022-11-25 09:07:16 -0400

    Modified Docker to include node_modules in a volume

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,6 +4,6 @@ router.use('/message', require('./message'));
 router.use('/notification', require('./notification'));
 router.use('/security', require('./security'));
 router.use('/user', require('./user'));
-router.get('/', (req, res) => { return res.status(200).json({ message: "Welcto the API" }) });
+router.get('/', (req, res) => { return res.status(200).json({ message: "Welcome to the API" }) });
 
 module.exports = router;
