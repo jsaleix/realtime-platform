04c9254a6def9045de17daf929abaad00345c38c
= <julian.saleix@gmail.com>
2022-11-30 22:32:38 +0100

    Adding some responsiveness

--- app/src/components/room-item/room-item.jsx
+++ app/src/components/room-item/room-item.jsx
@@ -5,18 +5,20 @@ export default function RoomItem({ room, onClick }) {
   return (
     <div className={style.main}>
       <h2>{room.name}</h2>
-      <p>
-        {room.users}/{room.maxUsers}
-      </p>
-      {room.users < room.maxUsers ? (
-        <button className={"btn blue"} onClick={onClick}>
-          Join
-        </button>
-      ) : (
-        <button className={"btn blue"} disabled>
-          Full
-        </button>
-      )}
+      <div className={style.information}>
+        <p>
+          {room.users}/{room.maxUsers}
+        </p>
+        {room.users < room.maxUsers ? (
+          <button className={"btn blue"} onClick={onClick}>
+            Join
+          </button>
+        ) : (
+          <button className={"btn blue"} disabled>
+            Full
+          </button>
+        )}
+      </div>
     </div>
   );
 }
